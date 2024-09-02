7cc5011b99ba7b698fa6d25140910fcc8fda2804
sasidhar6025 <sasi.dhar6025@gmail.com>
2024-09-02 20:17:14 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from langchain_openai import OpenAI
+import streamlit as st
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv() ## to load  the environment varaibles
+
+print(os.getenv("OPENAI_API_KEY"))
+def get_openai_response(question):
+    llm=OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"),temperature=0.5)
+    response=llm(question)
+    return response
+
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Langchain Application")
+
+input=st.text_input("Input: ",key='input')
+
+response=get_openai_response(input)
+
+submit=st.button("Ask the question ")
+
+if submit:
+    st.subheader("The Response is")
+    st.write(response)
